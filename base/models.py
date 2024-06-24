e87c9284f98a9dc5194367baa129b7bff88f9ccd
j4s3h <johncarlsalao304@gmail.com>
2024-06-24 14:59:12 +0800

    unique = True for Category

--- base/models.py
+++ base/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 class Category(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, unique  = True)
 
     def __str__(self):
         return self.name
